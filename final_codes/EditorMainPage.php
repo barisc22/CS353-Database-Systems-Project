ff4fd6fbd970730303a8d95de976abc1282eae50
mervekilicarslan5 <45542119+mervekilicarslan5@users.noreply.github.com>
2019-05-20 20:18:02 +0300

    Add files via upload

--- final_codes/EditorMainPage.php
+++ final_codes/EditorMainPage.php
@@ -14,6 +14,10 @@ $row4 = mysqli_fetch_assoc($r4);
 $points = $row4['penalty_points'];
 $count =  $row4['question_count'];
 
+$totQuestion= "SELECT count(challenge_id) FROM coding_challenge WHERE e_id = '".$editId."' ";
+$tot = mysqli_query($con, $totQuestion);
+$q = mysqli_fetch_assoc($tot);
+$q1 = implode(",", $q);
 
 if(array_key_exists('val', $_POST))
 {
@@ -88,10 +92,12 @@ function validate()
 									echo "<p><li class= \"w3-border\" style=\"width: 30%; display: inline-block;\">".$user. "</li>
        					<li class= \"w3-border\" style=\"width: 30%; display: inline-block;\">".$challenge. "
        						</li>";
+
+       						echo " <a  class= \"w3-button w3-purple w3-round-large\" style=\"width: 30%;\" href=' checkQuestion.php?challenge={$challenge} & user={$user} '  class=\"w3-bar-item w3-button\">Grade</a>";
 						}	
 					}
 					?>
-					<a  class= "w3-button w3-purple w3-round-large" style="width: 30%;" href="<?php echo " checkQuestion.php?challenge={$challenge} & user={$user} "?>" class="w3-bar-item w3-button">Grade</a>
+					
 
 					</ul>
 					<br><br>
@@ -135,7 +141,7 @@ function validate()
 			<br>
 			<img src="images/user1.png" style="width: 50%; height: 100%; padding-top: 50px;">
 			<p><label>Name : <?php echo "$name" ?></label>
-				<p><label>Question # : <?php echo "$count" ?></label>
+				<p><label>Question # : <?php echo "$q1" ?></label>
 			<p><label style="background-color: red">Penalty Points: <?php echo "$points" ?></label>
 			<br><br><br><br><br><br><br><br><br><br><br><br><br>
 			
