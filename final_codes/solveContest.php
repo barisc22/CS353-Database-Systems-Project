f36a3f90bf536890245de3055d8b69311ccd62a9
Fazilet Simge ER <33057057+simge98@users.noreply.github.com>
2019-05-21 13:20:08 +0300

    Update solveContest.php

--- final_codes/solveContest.php
+++ final_codes/solveContest.php
@@ -67,7 +67,7 @@ if(isset($_POST['answer'])){
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<link rel="stylesheet" type="text/css" href="styles/w3.css">
-	<title>CodeInt - Solve Challenge</title>
+	<title>CodeInt - Solve Contest</title>
 	<style>
 	/* width */
 	::-webkit-scrollbar {
