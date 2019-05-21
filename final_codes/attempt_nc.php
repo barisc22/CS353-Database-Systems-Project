c5f704430062596d3ce61e9ba150405f561d76dd
Fazilet Simge ER <33057057+simge98@users.noreply.github.com>
2019-05-21 13:11:05 +0300

    Update attempt_nc.php

--- final_codes/attempt_nc.php
+++ final_codes/attempt_nc.php
@@ -21,7 +21,7 @@ else{
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<link rel="stylesheet" type="text/css" href="styles/w3.css">
-	<title>CodeInt - Attempt</title>
+	<title>CodeInt - Attempt NC</title>
 	<style>
 	/* width */
 	::-webkit-scrollbar {
